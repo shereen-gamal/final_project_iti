64960c2cdfca53f331cdc826c1d914ad1124ca59
amirahamada12345 <amirahamada050@gmail.com>
2022-02-06 21:10:04 +0200

    api update user

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::put('/users/{user}',[UserController::class, 'update'])->name('users.update');
