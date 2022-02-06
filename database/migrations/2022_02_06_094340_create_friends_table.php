8f64adadf09dfc6bd97ae67f9489827e00871c44
shereen_28 <gmshereen98@gmail.com>
2022-02-06 12:51:56 +0200

    friends table commit

---
+++ database/migrations/2022_02_06_094340_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            
+            $table->id();
+            $table->foreignId('user_id')
+            ->nullable()
+            ->constrained();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
