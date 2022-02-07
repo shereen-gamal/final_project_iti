75a912381d248ecea14cdd3eb6bdeaf7f9faf0c6
shereen_28 <gmshereen98@gmail.com>
2022-02-07 14:38:24 +0200

    commit

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//user Routes
 Route::post('/user/token', [UserController::class,'Login']);/****************token_aPi******/
 Route::put('/users/{user}',[UserController::class, 'update'])->middleware('auth:sanctum');
 Route::get('/users',[UserController::class,'index'])->middleware('auth:sanctum');
 Route::get('/users/{user}',[UserController::class,'show'])->middleware('auth:sanctum');
 Route::delete('/users/{user}',[Usercontroller::class,'destroy'])->middleware('auth:sanctum');
-Route::post('/users', [UserController::class,'store'])->middleware('auth:sanctum');
+Route::post('/users', [UserController::class,'store']);
+
+//post Routes
 Route::get('posts',[postController::class,'index'])->name('APi'.' api.posts.index')->middleware('auth:sanctum');
 Route::get('posts/{post}',[postController::class,'show'])->middleware('auth:sanctum');
 Route::post('/posts',[PostController::class,'store'])->middleware('auth:sanctum');
