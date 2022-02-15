ccc15d53940669d4f308a0019210dd7552107ec2
shereen_28 <gmshereen98@gmail.com>
2022-02-16 01:21:29 +0200

    chat apis third edition for messages

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,7 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
-    protected $fillable=['chat_id','content','from_user_id','to_user_id'];
+    protected $fillable=[
+    'chat_id',
+    'content',
+    'from_user_id',
+    'to_user_id'];
    
     public function chat(){
         $this->belongsTo(Chat::class);
